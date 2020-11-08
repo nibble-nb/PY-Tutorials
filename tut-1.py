79d0e8a88a87ca67abde03f11bd459ae1c483e59
nibble-nb <74096501+nibble-nb@users.noreply.github.com>
2020-11-08 11:34:02 +0530

    coded  tut-1.py

--- tut-1.py
+++ tut-1.py
@@ -1,4 +1,6 @@
+# tut - 1
 print("Hello World!")     # Hello World!
 print("Hello","World!")   # Hello World!
 print("Hello"+"World!")   # HelloWorld!
 #  NIBBLE
+# print your name & comment in comment box below
