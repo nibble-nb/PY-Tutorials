d72ee3e163923ed529add1af0fc364f96bfb59c5
nibble-nb <74096501+nibble-nb@users.noreply.github.com>
2021-01-14 19:23:19 +0530

    coded tut-3.py

---
+++ tut-3.py
@@ -0,0 +1,26 @@
+integer  = 19
+string = "NIBBLE"
+floating = 16.69
+
+print(integer,type(integer))
+print(string,type(string))
+print(floating,type(floating))
+
+# Variables are the container, containing the value in them.
+
+integer = 10
+print(integer,string,floating)
+
+
+# NIBBLE 
+# NIBBLE
+# PLEASE LIKE THE VIDEO 
+# PLEASE SHARE THE VIDEO 
+# PLEASE SUBSCIRBE TO MY CHANNEL 
+# ITS FREE - THANKS GUYS :)
+
+# CODE LINK IN COMMENT SECTION BELOW
+# NIBBLE 
+# THANKS GUYS !!!!!
+
+# ASSIGNMENT ALERT : print your name as string & age as int using variables
