820ba4a2dcc5ddb83f18cadceabaec3a07403409
nibble-nb <74096501+nibble-nb@users.noreply.github.com>
2021-01-31 14:54:47 +0530

    Create tut-4.py

---
+++ tut-4.py
@@ -0,0 +1,34 @@
+# Single Line Comment - Only 1 Line commented
+
+'''
+Multi Line Comment - Many Lines are commented
+Multi Line Comment 
+'''
+
+
+
+
+# NIBBLE 
+# NIBBLE
+# PLEASE LIKE THE VIDEO 
+# PLEASE SHARE THE VIDEO 
+# PLEASE SUBSCIRBE TO MY CHANNEL 
+# ITS FREE - THANKS GUYS :)
+
+# CODE LINK IN COMMENT SECTION BELOW
+# NIBBLE 
+# THANKS GUYS !!!!!
+
+
+
+
+
+
+
+
+
+
+
+# $$$ -->  
+# ASSIGNMENT ALERT : add necessary comments in all previous codes and COMMENT IN COMMENT SECTION 
+# $$$
